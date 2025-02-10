86db11c3da4e8d26b690b48fa99aaec0b9e2caf4
jeffersondavila <jeffersondavila16@gmail.com>
2025-02-09 19:27:52 -0600

    [ADD] Se agrega el codigo en su estado actual

--- FrontBlogPersonal/Program.cs
+++ FrontBlogPersonal/Program.cs
@@ -1,4 +1,6 @@
+using Blazored.LocalStorage;
 using FrontBlogPersonal;
+using FrontBlogPersonal.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var urlApi = builder.Configuration.GetValue<string>("urlApi");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(urlApi!) });
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IBlogService, BlogService>();
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
